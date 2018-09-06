d5d118f046487b69922f35aa1b9ba185fc0cbf9c
Josiah Cal <42591872+jjckv6@users.noreply.github.com>
2018-09-06 12:49:20 -0500

    Declared variables and compared two samples

---
+++ Workspace.cs
@@ -0,0 +1,24 @@
+using system; 
+class Csharp-Basics;
+  
+  static void main()
+  {
+    const byte sample1 = 0x3A;
+    byte sample2 = 58;
+    int heartRate = 85;
+    double deposits = 135002796;
+    const float acceleration = 9.800;
+    float mass = 14.6;
+    double distance = 129.763001;
+    bool lost = true;
+    bool expensive = true;
+    const char integral = \u222B;
+    const string greeting = "Hello";
+    string name = "Karen";
+    
+      if(sample1 == sample2) {
+      Console.WriteLine("The Samples are equal.\n");
+      }
+      else {
+      Console.WriteLine("The Samples are not equal.\n");
+      }
